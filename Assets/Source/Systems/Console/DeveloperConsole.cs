6c85637520fb775a3ab69cb46d497452e92fab12
onlinesupergamer <bennyguybills@gmail.com>
2025-09-20 04:46:05 -0600

    Lots of Updates

--- Assets/Source/Systems/Console/DeveloperConsole.cs
+++ Assets/Source/Systems/Console/DeveloperConsole.cs
@@ -1,5 +1,17 @@
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
+
+/*
+ *  This should be a persistant class; It exists at all times!
+ *
+ *
+ *
+ */
+
+
+
 
 public class DeveloperConsole : MonoBehaviour
 {
@@ -8,20 +20,57 @@ public class DeveloperConsole : MonoBehaviour
     public TMPro.TMP_InputField ConsoleInput;
     public GameObject ConsolePanel;
     public Player PlayerCharacter;
+    public TMPro.TextMeshProUGUI ConsoleText;
 
     string InputText;
     List<string> PrevCommands;
+    int PrevCommandIndex = 0;
 
     private void Start()
-    {
+    {   
+        PrevCommands = new List<string>();
         ConsoleInput.onSubmit.AddListener(SubmitInput);
+        
     }
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.UpArrow) && PrevCommands.Count > 0) 
+        PrevConsoleCommands();
+        
+
+        if (Input.GetKeyDown(KeyCode.Escape)) 
         {
-            int PrevCommandIndex;
+            ConsolePanel.SetActive(false);
+            bIsConsoleOpen = false;
+            PlayerCharacter.bCanUserInput = true;
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
+    }
+
+    void PrevConsoleCommands() 
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow) && PrevCommands.Count != 0)
+        {
+            PrevCommandIndex--;
+            PrevCommandIndex = Mathf.Clamp(PrevCommandIndex, 0, PrevCommands.Count - 1);
+
+            ConsoleInput.text = PrevCommands[PrevCommandIndex];
+            ConsoleInput.caretPosition = PrevCommands[PrevCommandIndex].Length;
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.DownArrow) && PrevCommands.Count != 0) 
+        {
+            PrevCommandIndex++;
+            PrevCommandIndex = Mathf.Clamp(PrevCommandIndex, 0, PrevCommands.Count - 1);
+
+            ConsoleInput.text = PrevCommands[PrevCommandIndex];
+            ConsoleInput.caretPosition = PrevCommands[PrevCommandIndex].Length;
+
+
+            //What the fuck is even happening here?
+
         }
     }
 
@@ -35,13 +84,13 @@ public class DeveloperConsole : MonoBehaviour
 
             ProcessInput(command, parts);
 
-            //PrevCommands.Add(SubmittedText);
+            PrevCommands.Add(SubmittedText);
+            ConsoleText.text += SubmittedText + "\n";
+            PrevCommandIndex = PrevCommands.Count;
             ConsoleInput.text = "";
             ConsoleInput.ActivateInputField();
 
         }
-
-
     }
 
     void ProcessInput(string _command, string[] _args) 
@@ -52,18 +101,19 @@ public class DeveloperConsole : MonoBehaviour
         switch (_command) 
         {
             case "set_fov":
-                int MaxArgs = 2; //This includes initial command
+                int MaxArgs = 2; //This is what arg lengths are checked by (Includes the actual command at index 1, with args starting at 2)
 
                 if (_args.Length > 2) 
                 {
-                    print("Too Many Arguments!");
+                    ConsoleLog("Too Many Arguments!");
                     return;
                 }
 
                 if (float.TryParse(_args[1], out float fov)) 
                 {
-                    print($"FOV set to {fov}!");
+                    //ConsoleText.text += "Setting main camera FOV\n";
                     PlayerCharacter.SetCameraFOV(fov);
+                    ConsoleLog("Camera FOV Set");
                 }
 
             break;
@@ -75,11 +125,14 @@ public class DeveloperConsole : MonoBehaviour
                 }
                 if(float.TryParse(_args[1], out float weaponfov)) 
                 {
+                    //ConsoleText.text += "Setting first person viewmodel FOV\n";
                     PlayerCharacter.SetViewmodelFOV(weaponfov);
+                    ConsoleLog("Viewmodel Set");
                 }
-                    break;
-
+                break;
         }
+
+        //Remember to log command outputs into the console! -----------------------------------
     }
 
     public void OpenConsole() 
@@ -89,6 +142,8 @@ public class DeveloperConsole : MonoBehaviour
             ConsolePanel.SetActive(false);
             bIsConsoleOpen = false;
             PlayerCharacter.bCanUserInput = true;
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
         }
         else 
         {
@@ -99,5 +154,11 @@ public class DeveloperConsole : MonoBehaviour
             Cursor.visible = true;
             ConsoleInput.ActivateInputField();
         }
+    }
+
+    public void ConsoleLog(string ConsoleFeedback) 
+    {
+        ConsoleText.text += "Console: " + ConsoleFeedback + "\n";
+
     }
 }
